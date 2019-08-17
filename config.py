3e90cd4aa29b883c5a03d24462718bb8161477b6
Towernter <tnyahuye@axissol.com>
2019-08-17 13:33:17 +0200

    coyg commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import tweepy
+import logging
+from os import environ
+
+logger = logging.getLogger()
+
+
+def create_api():
+    consumer_key = environ['consumer_key']
+    consumer_secret = environ['consumer_secret']
+    access_token = environ['access_token']
+    access_token_secret = environ['access_token_secret']
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
+
+
---
+++ mybot.py
@@ -0,0 +1,38 @@
+from tweepy import StreamListener
+from tweepy import Stream
+import logging
+from config import create_api
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+class FavRetweetListener(StreamListener):
+    def __init__(self, api=None):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        logger.info(f"Processing tweet id {tweet.id}")
+        if tweet.in_reply_to_status_id is not None or \
+            tweet.user.id == self.me.id:
+            return
+        try:
+            tweet.favorite()
+            #tweet.retweet()
+        except Exception as e:
+            logger.error("Error on fav and retweet", exc_info=True)
+
+    def on_error(self, status):
+        logger.error(status)
+
+
+def main(keywords):
+    api = create_api()
+    tweets_listener = FavRetweetListener(api)
+    stream = Stream(api.auth, tweets_listener)
+    stream.filter(track=keywords, languages=["en"])
+
+
+if __name__ == "__main__":
+    main(["#Arsenal", "#COYG", "AFC", "arsenalfc"])
