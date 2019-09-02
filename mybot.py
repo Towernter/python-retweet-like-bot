326e10ff2fc3769fe1d103c873e53647f821feb3
towernter <32792087+Towernter@users.noreply.github.com>
2019-09-02 07:40:11 +0200

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -21,7 +21,7 @@ class FavRetweetListener(StreamListener):
         try:
             tweet.retweet()
             tweet.favorite()
-            time.sleep(150)
+            time.sleep(200)
         except Exception as e:
             logger.error("Error on fav and retweet", exc_info=True)
         
