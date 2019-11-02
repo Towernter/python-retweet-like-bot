f7f57432276d5b9ba11640c0b224e0b04792d8e6
towernter <32792087+Towernter@users.noreply.github.com>
2019-11-02 12:20:48 +0200

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -5,10 +5,19 @@ from config import create_api
 import datetime
 import time
 
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-blocked_people = ["lawrhuns", "fan_banter", "_whatthesport", "bitcoinconnect", "premtrackerapp", "zakariaacehgrup", "e_arsenalnet", "footy90com"]
-blocked_words = ["@BudweiserNG", "gay", "lgbt", "fuck", "cunt", "pussy","lesben","dick","LGBT", "RT @e_arsenalnet"]
+blocked_people = ["lawrhuns", "fan_banter",
+                    "_whatthesport", "bitcoinconnect",
+                    "premtrackerapp", "zakariaacehgrup",
+                    "e_arsenalnet", "footy90com"
+                    ]
+blocked_words = ["@BudweiserNG", "gay", "lgbt",
+                 "fuck", "cunt", "pussy","lesben",
+                 "dick", "RT @", "lesbian",
+                 "LGBT","RT @e_arsenalnet",
+                 ]
 
 
 class FavRetweetListener(StreamListener):
@@ -16,19 +25,51 @@ class FavRetweetListener(StreamListener):
         self.api = api
         self.me = api.me()
 
+    def contains_hate_words(self, tweet):
+        for word in blocked_words:
+            if word in tweet:
+                return True
+            else:
+                return False
+
+    def is_old(self, tweet):
+        if (datetime.datetime.now() - tweet.created_at).days > 0.016666667:
+            return True
+        else:
+            return False
+
+    def is_reply(self, tweet):
+        if tweet.in_reply_to_status_id is not None:
+            return True
+        else:
+            return False
+
+    def i_tweeted(self, tweet):
+        if tweet.user.id == self.me.id:
+            return True
+        else:
+            return False
+
+    def tweeted_by_blocked_user(self, tweet):
+        if tweet.user.screen_name in blocked_people:
+            return True
+        else:
+            return False
+
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
-        if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id or (datetime.datetime.now() - tweet.created_at).days > 0.01666666666 or \
-            tweet.user.screen_name in blocked_people or [word for word in blocked_words if word in tweet.text]:
+
+        if self.contains_hate_words(tweet.text) or self.is_old(tweet) or self.is_reply(tweet) or \
+            self.i_tweeted(tweet) or self.tweeted_by_blocked_user(tweet):
             return
-        try:
-            tweet.retweet()
-            tweet.favorite()
-            time.sleep(5)
-        except Exception as e:
-            logger.error("Error on fav and retweet", exc_info=True)
-        
+
+        else:
+            try:
+                tweet.retweet()
+                tweet.favorite()
+                time.sleep(5)
+            except Exception as e:
+                logger.error("Error on fav and retweet", exc_info=True)
 
     def on_error(self, status):
         logger.error(status)
@@ -39,7 +80,6 @@ def main(keywords):
     tweets_listener = FavRetweetListener(api)
     stream = Stream(api.auth, tweets_listener)
     stream.filter(track=keywords, languages=["en"])
-
 
 if __name__ == "__main__":
     main(["#Arsenal", "#COYG"])
