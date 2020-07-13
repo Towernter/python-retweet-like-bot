47f7d79a50fe4e74da0d583b0370b3124f34e890
towernter <towernter@gmail.com>
2020-07-13 20:03:05 +0200

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -3,13 +3,13 @@ from tweepy import Stream
 import logging
 from config import create_api
 import datetime
-import time;;;
+import time
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-blocked_people = ["user1", "user...n"]
-blocked_words = ["word1", "word...n"]
+blocked_people = ["user1", "user...n"] #enter users you want to block from retweeting their tweets here
+blocked_words = ["word1", "word...n"] #enter words you want to block here
 
 
 class FavRetweetListener(StreamListener):
@@ -23,31 +23,36 @@ class FavRetweetListener(StreamListener):
                 return True
             else:
                 return False
-
+            
+    #Check if the tweet is old //you can adjust the time
     def is_old(self, tweet):
         if (datetime.datetime.now() - tweet.created_at).days > 0.016666667:
             return True
         else:
             return False
-
+    
+    #check if a tweet is a reply you can remove this method if you want to retweet replies
     def is_reply(self, tweet):
         if tweet.in_reply_to_status_id is not None:
             return True
         else:
             return False
-
+    
+    #Avoid retweeting your own tweets
     def i_tweeted(self, tweet):
         if tweet.user.id == self.me.id:
             return True
         else:
             return False
-
+    
+    #check if the tweet is from a blocked user
     def tweeted_by_blocked_user(self, tweet):
         if tweet.user.screen_name in blocked_people:
             return True
         else:
             return False
 
+    #this method handles all the tweeting    
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
 
@@ -66,7 +71,7 @@ class FavRetweetListener(StreamListener):
     def on_error(self, status):
         logger.error(status)
 
-
+#our main function
 def main(keywords):
     api = create_api()
     tweets_listener = FavRetweetListener(api)
